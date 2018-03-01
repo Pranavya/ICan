979a0461c1093ce24e53d415727bf30592b13bea
Pranavya <lomdom12345@gmail.com>
2018-03-01 12:40:32 +0530

    Added Floating Action Button

--- app/src/main/java/com/pranavya/ican/Home.java
+++ app/src/main/java/com/pranavya/ican/Home.java
@@ -1,10 +1,14 @@
 package com.pranavya.ican;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +18,7 @@ import android.widget.Toast;
 
 import java.util.Locale;
 
-/**
- * Created by Sucharita on 24-02-2018.
- */
+
 
 public class Home extends Fragment implements View.OnClickListener{
 
@@ -33,17 +35,24 @@ public class Home extends Fragment implements View.OnClickListener{
         ib1 =(ImageButton) v.findViewById(R.id.imageButton);
         ed1 = (EditText) v.findViewById(R.id.editText2);
 
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Home.this.getActivity(),GestureActivity.class);
+                startActivity(i);
+            }
+        });
+
         t1 = new TextToSpeech(getActivity().getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
+                if (status != TextToSpeech.ERROR ) {
                     t1.setLanguage(Locale.UK);
                 }
             }
         });
         ib1.setOnClickListener(this);
-
-
         return v;
     }
     public void onButtonPressed(Uri uri) {
@@ -52,23 +61,17 @@ public class Home extends Fragment implements View.OnClickListener{
         }
     }
 
-
     @Override
     public void onClick(View view) {
-        //if (view == ib1) {
-            String toSpeak = ed1.getText().toString();
+        String toSpeak = ed1.getText().toString();
+        String error = "Write text to speak";
+        if(toSpeak.isEmpty()){
+            Toast.makeText(getActivity().getApplicationContext(),error,Toast.LENGTH_SHORT).show();
+        }
+        else {
             Toast.makeText(getActivity().getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
             t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
-        //}
-        //else if (view == b2) {
-            //Intent intent = new Intent(this, SaveGenres.class);
-            //Intent intent = new Intent(Home.this.getActivity(), SaveGenres.class);
-          //  startActivity(intent);
-            //Intent intent = new Intent(Home.this.getActivity(), Main2Activity.class);
-            //ed2 = (EditText) view.findViewById(R.id.editText2);
-            //intent.putExtra("text", ed2.getText().toString());
-            //startActivity(intent);
-        //}
+        }
     }
 
     public interface OnFragmentInteractionListener {
